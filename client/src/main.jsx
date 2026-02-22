814681d666b215f7e196aa11e8808af53dc38936
Chizaram-Igolo <emmanuel.igolo@nextbasket.com>
2026-02-22 23:27:11 +0100

    feat: Add Dark Mode Toggle (#3)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'
 import { HashRouter } from 'react-router-dom';
+import { ThemeProvider } from './ThemeContext';
 import './index.css';    // Optional global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <HashRouter> {/* or HashRouter if deploying to GitHub Pages */}
-    <App />
+    <HashRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </HashRouter>
   </React.StrictMode>
 );
